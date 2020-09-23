4d00708775a0e266fa5d309de6bdb008bd382fd9
Vitaliy Iliuk <auselesszzz@outlook.com>
2020-09-24 02:24:51 +0300

    fixed world and added adress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Woorld!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
