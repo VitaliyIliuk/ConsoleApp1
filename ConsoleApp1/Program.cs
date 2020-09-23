752dfd53e8462b582edd6a483e5cf032d9a14c8b
Vitaliy Iliuk <auselesszzz@outlook.com>
2020-09-24 02:19:15 +0300

    Added a new class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,10 @@ namespace ConsoleApp1
 {
     class Program
     {
+        class MyClass
+        {
+            string name { get; set; }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Woorld!");
